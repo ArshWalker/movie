774f25ab8090c6d7acbbc10737008c9d4fbc3ee8
ArshWalker <arshwalker7@gmail.com>
2021-08-01 23:54:12 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header></Header>
+      <div className="app">Movies Data </div>
+      <SimpleBottomNavigation></SimpleBottomNavigation>
+    </>
+  );
 }
 
 export default App;
