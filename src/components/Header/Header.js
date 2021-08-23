7df94c025c927e4b10205c13f37618d3d26eeaaf
ArshWalker <arshwalker7@gmail.com>
2021-08-24 03:48:35 +0530

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <span className="header">🎥 Movie World 🎬</span>
+      <span onClick={() => window.scroll(0,0)} className="header">🎥 Movie World 🎬</span>
     </div>
   );
 };
