f3922e53cd2eb59facbc253408ae84a795a65712
ArshWalker <arshwalker7@gmail.com>
2021-08-24 10:00:44 +0530

    trending page updated

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -24,7 +24,7 @@ const Trending = () => {
 
      return (
           <div>
-              <span className="pageTitle">Trending</span>
+              <span className="pageTitle">Weekly Trending</span>
               <div className="trending">
                    {
                         content && content.map((c) => (
