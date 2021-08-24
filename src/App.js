42c43698af8034b8dfcc2e216af1633ea1ed619f
ArshWalker <arshwalker7@gmail.com>
2021-08-24 09:58:36 +0530

    toggle updated

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import SimpleBottomNavigation from "./components/MainNav";
 import Trending from "./Pages/Trending/Trending";
 import Movies from "./Pages/Movies/Movies";
 import Series from "./Pages/Series/Series";
-
 import Search from "./Pages/Search/Search";
 
+import { Toggle } from './components/Toggle';
+import { useDarkMode } from './styles/useDarkMode';
+import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
+import  { ThemeProvider } from 'styled-components';
+
 function App() {
+
+  const [ theme, toggleTheme ] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <BrowserRouter>
-      <Header></Header>
-      <div className="app">
+    
+     <ThemeProvider theme={themeMode} className="toggle">
+     <Header></Header>
+     <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={toggleTheme} className="toggle"/>
+      <div className="app stripe">
         <Container>
           <Switch>
           <Route path="/" component={Trending} exact />
@@ -24,6 +36,8 @@ function App() {
         </Container>
       </div>
       <SimpleBottomNavigation></SimpleBottomNavigation>
+     </ThemeProvider>
+      
     </BrowserRouter>
   );
 }
