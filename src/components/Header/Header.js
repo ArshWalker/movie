f9bcfbf4bc3162aa4a5ab0a6150989b2b7b09601
ArshWalker <arshwalker7@gmail.com>
2021-08-24 10:03:03 +0530

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,10 @@ import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <span onClick={() => window.scroll(0,0)} className="header">🎥 Movie World 🎬</span>
+    <div onClick={() => window.scroll(0,0)} className="sign">
+      {/* <span  onClick={() => window.scroll(0,0)} className="header">🎥 Movie World 🎬</span> */}
+      <span  className="header" class="fast-flicker">Mov</span>ie <span class="flicker">Wor</span>ld
+    </div>
     </div>
   );
 };
